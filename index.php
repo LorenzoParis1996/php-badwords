a09a8a3aab11d7318071a9bc62241ae03d2674cf
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-25 15:36:23 +0200

    testando php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = "gigi";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <h1>ciao</h1>
+    <p>Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you <?php echo $name ?></p>
+</body>
+</html>
